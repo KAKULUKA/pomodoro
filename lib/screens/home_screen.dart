a241d6377c6a8a98e8e3fd482b504ef8f428553f
KAKULUKA <hardwords@naver.com>
2023-09-21 21:16:02 +0900

    POMODORO

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,15 +16,64 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           Flexible(
             flex: 1,
-            child: Container(),
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '25:00',
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
           ),
           Flexible(
             flex: 2,
-            child: Container(),
+            child: Container(
+              child: Center(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.play_circle_outlined),
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                ),
+              ),
+            ),
           ),
           Flexible(
             flex: 1,
-            child: Container(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).cardColor),
+                    child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pomodoros',
+                            style: TextStyle(
+                              color: Color(0XFF232B55),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              color: Color(0XFF232B55),
+                              fontSize: 58,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
